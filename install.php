fb73ddba49b18a494c253784e73b16ed7b35f117
Anastasia Keksikova <130557226+Devo4ka-dev@users.noreply.github.com>
2024-01-31 02:58:57 +0100

    Update install.php

--- install.php
+++ install.php
@@ -2,7 +2,7 @@
 header('Content-Type: text/plain');
 
 $file_content .= "sudo echo \"#!/bin/bash\" > /usr/bin/kmi\n";
-$file_content .= "sudo echo \"curl -F 'kmi=<-' https://kmi.devo4ka.top/\" >> /usr/bin/kmi\n";
+$file_content .= "sudo echo \"curl -F 'kmi=<-' https://kmi.devo4ka.top/kmi\" >> /usr/bin/kmi\n";
 $file_content .= "sudo chmod +x /usr/bin/kmi\n";
 
 echo $file_content;
